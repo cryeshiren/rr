17f780b6a66cf6817ed04681fa16a7337454a258
dong <cryeshiren@163.com>
2017-12-24 09:28:11 +0800

    add handle menthod for searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,15 +6,42 @@ export interface SearchBarProps {
     onSearch: (text: string) => void
 }
 
-export default class SearchBar extends React.Component<SearchBarProps, any> {
+export interface SearchBarState {
+    text: string
+}
+
+export default class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
     constructor(props) {
         super(props);
+        this.state = { text: '' };
     }
+
+    handleOnChange(event) {
+        event.preventDefault();
+        this.setState({text: event.target.value});
+    }
+
+    handleOnClick(event) {
+        event.preventDefault();
+        this.props.onSearch(this.state.text.trim());
+        this.setState({ text: '' });
+    }
+
     render() {
         return (
             <div>
-                <TextField id="search" label={this.props.label} type="search" margin="normal"/>
-                <Button raised type="submit">Search</Button>
+                <TextField 
+                    id="search" 
+                    label={this.props.label} 
+                    type="search" 
+                    margin="normal"
+                    onChange={(event) => this.handleOnChange(event)}/>
+                <Button 
+                    raised 
+                    type="submit"
+                    onClick={(event) => this.handleOnClick(event)}>
+                    Search
+                </Button>
             </div>
         );
     }
