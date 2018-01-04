171761345aeb2796ec344ef10717062a0dde3619
cryeshiren <cryeshiren@163.com>
2018-01-04 17:29:57 +0800

    Implement douban movie

--- src/components/DoubanMovie/MovieGridList.tsx
+++ src/components/DoubanMovie/MovieGridList.tsx
@@ -1,27 +1,32 @@
 import * as React from 'react';
 import { MovieModel } from '../../models/MovieModel';
 import { GridList, GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Subheader from 'material-ui/List/ListSubheader';
 
 export interface MovieGridListProps {
-    movies: Array<MovieModel>
+    movies: Array<MovieModel>;
+    cols: number;
+    cellHeight: number;
+    imageHeight: number;
+    imageWidth: number;
 }
 
 export default class MovieGridList extends React.Component<MovieGridListProps, any> {
-  render() {
-    return (
-      <div>
-        <GridList>
-          {this.props.movies.map((movie, index) => {
-            <GridListTile key={index}>
-              <img src={movie.image}/>
-              <GridListTileBar
-                title={movie.title}
-                subtitle={<span>导演：{movie.director.name}</span>}
-              />
-            </GridListTile>
-          })}
-        </GridList>        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div >
+                <GridList cols={this.props.cols} cellHeight={this.props.cellHeight}>
+                    {this.props.movies.map(movie => (
+                        <GridListTile key={movie.image}>
+                        <img src={movie.image} alt={movie.title} width={this.props.imageWidth} height={this.props.imageHeight}/>
+                        <GridListTileBar
+                            title={movie.title}
+                            subtitle={<span>by: {movie.director.name}</span>}
+                        />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+        );
+    }
 }
--- src/containers/DoubanMovieContainer.tsx
+++ src/containers/DoubanMovieContainer.tsx
@@ -6,36 +6,47 @@ import { getMovieData } from "../actions/DoubanMovieAction";
 import { MovieModel, Rating, Actor, Director } from "../models/MovieModel";
 
 interface DoubanMovieContainerState {
-  movies: Array<MovieModel>
+  movies: Array<MovieModel>;
+  city?: string;
 }
 
 export default class DoubanMovieContainer extends React.Component<any, DoubanMovieContainerState> {
   constructor(props) {
     super(props);
-    this.state = {movies: []};
+    this.state = {movies: [], city:""};
   }
 
   render() {
     return (
-      <div>
+      <div style={{
+        overflow: 'hidden',
+        width: 1782,
+        margin: '0 auto'
+      }}>
         <SearchBar label="请输入需要查看的城市" onSearch={ text => this.search(text) }/>
-        <MovieGridList movies={this.state.movies}/>
+        <MovieGridList movies={this.state.movies} cellHeight={297} imageHeight={297} imageWidth={212} cols={6}/>
       </div>
     );
   }
 
-  search(city: string) {
-    return getMovieData(city)
-    .then(response => response.data)
+  async search(city: string) {
+    await getMovieData(city)
+    .then(response => response.data.subjects)
     .then(movies => {
       let result = movies.map(movie => {
         let m = new MovieModel();
-        m = {...movie};
+        m.rating = new Rating(movie.rating.max, movie.rating.average, movie.rating.min);
+        m.type = {...movie.genres};
+        m.title = movie.title;
+        m.director = new Director(movie.directors[0] && movie.directors[0].name);
+        m.actors = movie.casts.map(c => {
+          return new Actor(c.name, c.alt, c.avatars.medium);
+        });
+        m.durations = movie.durations[0];
+        m.image = movie.images.medium;
+        m.pubDate = movie.pubdates[0];
         return m;
       });
-      console.log('====================================');
-      console.log(result);
-      console.log('====================================');
       this.setState({movies: result});
     });
   }
